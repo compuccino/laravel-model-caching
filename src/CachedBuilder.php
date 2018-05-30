cddedbb095a3a4059eb3f1d28cc0bbf312c71eb2
Grischa Andreew <grischa@grischaandreew.de>
2018-05-30 14:51:34 +0200

    fix pagination parameter

--- src/CachedBuilder.php
+++ src/CachedBuilder.php
@@ -126,7 +126,7 @@ class CachedBuilder extends EloquentBuilder
             return parent::paginate($perPage, $columns, $pageName, $page);
         }
 
-        $page = request("page", $page ?: 1);
+        $page = request("page[number]", $page ?: 1);
         $cacheKey = $this->makeCacheKey($columns, null, "-paginate_by_{$perPage}_{$pageName}_{$page}");
 
         return $this->cachedValue(func_get_args(), $cacheKey);
